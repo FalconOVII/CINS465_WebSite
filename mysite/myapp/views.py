e2f54dab30487d30061d8feaed65fb1afba435de
FalconOVII <icolio@mail.csuchico.edu>
2019-05-01 21:23:31 -0700

    before generic

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Recipe
+from .models import Recipe, Mix
 
 def index(request):
     all_recipes = Recipe.objects.all()
@@ -8,6 +8,20 @@ def index(request):
 def detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, 'detail.html', {'recipe': recipe })
+def favorite(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    try:
+        selected_mix = recipe.mix_set.get(pk=request.POST['mix'])
+    except(KeyError, Mix.DoesNotExist):
+        return render(request, 'myapp/detail.html',{
+            'recipe': recipe,
+            'error_message': 'You didn not select a Mix Drink Recipe',
+        })
+    else:
+        selected_mix.is_favorite = True
+        selected_mix.save()
+        return render(request,'myapp/details/html', {'recipe': recipe})
+
 '''
 from django.views import generic
 from .models import Recipe
